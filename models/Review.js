61d9693cefc021aff14560dca56731b42bf965ee
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-05-27 10:51:16 -0700

    Added review model

---
+++ models/Review.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: 2,
+  },
+  rating: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: 2,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: 2,
+  },
+  text: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: 2,
+  },
+});
+
+const Review = mongoose.model("Review", ReviewSchema);
+
+module.exports = Review;
