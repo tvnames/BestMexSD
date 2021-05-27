6b3fb5d122f63bf6a966dadb9cf27332422778f6
William Lucht <aaronlucht@gmail.com>
2021-05-27 11:43:47 -0700

    added new slides to carousel. Fixed responsive issues for large screens.

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import style from "./style.css";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 import taco1 from "./taco1.jpg";
 import taco2 from "./taco2.jpg";
 import taco3 from "./taco3.jpg";
+import taco4 from "./taco4.jpg";
+import taco5 from "./taco5.jpg";
 
 function Hero({ children }) {
   return (
@@ -23,16 +25,23 @@ function Hero({ children }) {
         autoplay: true,
         autoWidth: true,
         speed: 500,
+        focus: "center",
       }}
     >
       <SplideSlide>
         <img src={taco1} alt="taco shop hero" />
       </SplideSlide>
-      <SplideSlide>
+      {/* <SplideSlide>
         <img src={taco2} alt="taco hero close up" />
       </SplideSlide>
       <SplideSlide>
-        <img src={taco3} alt="taco hero close up" />
+        <img src={taco3} alt="taco hero close up number two" />
+      </SplideSlide> */}
+      <SplideSlide>
+        <img src={taco4} alt="taco hero close up number three" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src={taco5} alt="taco hero close up number four" />
       </SplideSlide>
     </Splide>
   );
