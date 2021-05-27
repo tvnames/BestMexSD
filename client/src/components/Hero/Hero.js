7325e43098d6ce026f8a48c2c2c5a8f811336ba8
William Lucht <aaronlucht@gmail.com>
2021-05-27 13:16:52 -0700

    hero and nav style are finalized.

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -10,15 +10,6 @@ import taco5 from "./taco5.jpg";
 
 function Hero({ children }) {
   return (
-    // <div class="jumbotron jumbotron-fluid">
-    //   <div class="container">
-    //     <h1 class="display-4">{children}</h1>
-    //     <p class="lead">
-    //       This is a modified jumbotron that occupies the entire horizontal space
-    //       of its parent.
-    //     </p>
-    //   </div>
-    // </div>
     <Splide
       options={{
         type: "loop",
@@ -26,6 +17,7 @@ function Hero({ children }) {
         autoWidth: true,
         speed: 500,
         focus: "center",
+        padding: 0,
       }}
     >
       <SplideSlide>
